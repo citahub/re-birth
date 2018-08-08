bb019b7bee149a3e690a94ed850f8a1027bfb503
classicalliu <classicalliu@gmail.com>
2018-08-08 16:09:56 +0800

    add not found handle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::RoutingError do |e|
+    render json: {
+      message: e.message
+    }, status: :not_found
+  end
+
+  def not_found
+    raise ActionController::RoutingError, "not found"
+  end
 end
