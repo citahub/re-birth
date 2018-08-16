dd8aa1281eb7a58e1e135266e601552eef500d34
classicalliu <classicalliu@gmail.com>
2018-08-16 12:44:56 +0800

    fix a bug: tendermint => Tendermint

--- app/serializers/meta_data_serializer.rb
+++ app/serializers/meta_data_serializer.rb
@@ -1,4 +1,11 @@
 class MetaDataSerializer < ActiveModel::Serializer
-  attributes :chain_id, :chain_name, :operator, :website, :genesis_timestamp, :validators, :block_interval, :token_name, :token_symbol, :token_avatar
+  attributes :operator, :website, :validators
+  attribute :chain_id, key: :chainId
+  attribute :chain_name, key: :chainName
+  attribute :genesis_timestamp, key: :genesisTimestamp
+  attribute :block_interval, key: :blockInterval
+  attribute :token_name, key: :tokenName
+  attribute :token_symbol, key: :tokenSymbol
+  attribute :token_avatar, key: :tokenAvatar
 
 end
--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,4 +1,6 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :content, :block_number, :block_hash, :index
+  attributes :content, :index
+  attribute :block_number, key: :blockNumber
+  attribute :block_hash, key: :blockHash
   attribute :cita_hash, key: :hash
 end
