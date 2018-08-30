932a2aa468c4df751e328ae8f54e9ef60ce77990
classicalliu <276055502@qq.com>
2018-08-30 13:49:18 +0800

    fix upgrade to cita v0.18 migration, drop old and create new one, for work with pg v9.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_041340) do
+ActiveRecord::Schema.define(version: 2018_08_30_054052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
