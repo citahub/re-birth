e4c76fcba11b10df956ca943d53c0a7cce127201
classicalliu <classicalliu@gmail.com>
2018-09-20 17:50:33 +0800

    update gems and fix it's problems

--- spec/factories/abis.rb
+++ spec/factories/abis.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :abi do
-    address "0x0dcf740686de1fe9e9faa4b519767a872e1cf69e"
-    value "0x"
-    block_number 0
+    address { "0x0dcf740686de1fe9e9faa4b519767a872e1cf69e" }
+    value { "0x" }
+    block_number { 0 }
   end
 end
