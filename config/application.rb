1305f114f0dd5c75ae454f4590600bbf2f90ed7a
classicalliu <classicalliu@gmail.com>
2018-10-29 19:00:03 +0800

    Move cors config from application.rb to initializers(Rails api only standard)

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,5 @@ module ReBirth
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # cors config
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :put, :delete]
-      end
-    end
   end
 end
