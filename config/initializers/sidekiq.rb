309ff73db1d92da71fa2f04005708a48508d6e25
classicalliu <classicalliu@gmail.com>
2018-11-22 15:37:47 +0800

    Set default redis namespace to nil.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,8 @@ redis_config = Rails.application.config_for(:redis)
 sidekiq_url = redis_config["url"]
 redis_password = redis_config["password"]
 
-namespace = ENV.fetch("REDIS_NAMESPACE") { "rebirth" }
+namespace = ENV.fetch("REDIS_NAMESPACE") { nil }
+namespace = nil if namespace.blank?
 
 Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_url, driver: :hiredis, password: redis_password, namespace: namespace }
